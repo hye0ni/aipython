6d7eba7604410c1e4b5fecf30fde1f93b2a110d0
seohyeon choi <shyeon0812@gmail.com>
2025-04-07 03:08:22 +0000

    함수만들기

--- test4.py
+++ test4.py
@@ -12,18 +12,19 @@ def hello_name(name):
 name = input("이름을 입력: ")
 hello_name(name)
 
-#연산을 하는 함수
-def cal(n1, n2, op): # 1, 2, + (이렇게 입력)
-   r = 0 # 결과값
-   if op == "+":
-    r = n1 + n2
+# 연산을 하는 함수
+def cal(n1, n2, op):  # 예: 1, 2, "+"
+    r = 0  # 결과값
+    if op == "+":
+        r = n1 + n2
     elif op == "-":
         r = n1 - n2
-        else:
-            print("잘못입력")
-        return r # 결과값을 전달
+    else:
+        print("잘못된 연산자입니다.")
+    return r  # 결과값을 전달
 
 r = cal(2, 1, "+")
-print(f"두 수를 더한 값{r}")
-cal(2, 1, "-")
-print(f"두 수를 뺀 값{r}")
+print(f"두 수를 더한 값: {r}")
+
+r = cal(2, 1, "-")
+print(f"두 수를 뺀 값: {r}")
