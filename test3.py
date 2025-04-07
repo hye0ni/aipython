a40220d35f9d1fa8136117a009c3f97c990069ca
seohyeon choi <shyeon0812@gmail.com>
2025-04-07 03:03:30 +0000

    함수만들기

---
+++ test3.py
@@ -0,0 +1,15 @@
+def print_ascii_art(option):
+    if option == 1:
+        print("""
+         /\_/\  
+        ( o.o ) 
+         > ^ <  
+        """)
+
+        for line in cat:
+        print(line)
+
+        for i in range(3):
+            print (i)
+            
+             # print_cat()
